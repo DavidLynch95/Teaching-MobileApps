5e19fe40d1788fa010f0459cc60152d894a8cf76
David Lynch <djl516@humboldt.edu>
2018-03-07 14:48:39 -0800

    Attempted to add high-res code but it doesn't work yet

--- projects/project 3/GoogleApiExample/MainActivity.cs
+++ projects/project 3/GoogleApiExample/MainActivity.cs
@@ -11,6 +11,16 @@ namespace GoogleApiExample
     [Activity(Label = "GoogleApiExample", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        /// <summary>
+        /// Used to track the file that we're manipulating between functions
+        /// </summary>
+        public static Java.IO.File _file;
+
+        /// <summary>
+        /// Used to track the directory that we'll be writing to between functions
+        /// </summary>
+        public static Java.IO.File _dir;
+
         List<string> tags = new List<string>();
         private string submittedAnswer;
         private Google.Apis.Vision.v1.Data.BatchAnnotateImagesResponse apiResult;
@@ -18,6 +28,10 @@ namespace GoogleApiExample
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            //see https://forums.xamarin.com/discussion/97273/launch-camera-activity-with-saving-file-in-external-storage-crashes-the-app
+            //I'm not sure if doing the two lines below is a great idea, but it does seem to fix passing files around
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.SetVmPolicy(builder.Build());
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
@@ -25,6 +39,7 @@ namespace GoogleApiExample
 
             if (IsThereAnAppToTakePictures() == true)
             {
+                CreateDirectoryForPictures();
                 FindViewById<Button>(Resource.Id.launchCameraButton).Click += TakePicture;
             }
         }
@@ -43,10 +58,25 @@ namespace GoogleApiExample
             return availableActivities != null && availableActivities.Count > 0;
         }
 
+        /// <summary>
+        /// Creates a directory on the phone that we can place our images
+        /// </summary>
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new Java.IO.File(
+                Android.OS.Environment.GetExternalStoragePublicDirectory(
+                    Android.OS.Environment.DirectoryPictures), "GoogleApiExample");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
+        }
 
         private void TakePicture(object sender, System.EventArgs e)
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
+            _file = new Java.IO.File(_dir, string.Format("myPhoto_{0}.jpg", System.Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
             StartActivityForResult(intent, 0);
         }
 
@@ -60,6 +90,12 @@ namespace GoogleApiExample
         {
             base.OnActivityResult(requestCode, resultCode, data);
 
+            //send to image gallery
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            var contentUri = Android.Net.Uri.FromFile(_file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
             // Display in ImageView. We will resize the bitmap to fit the display.
             // Loading the full sized image will consume too much memory
             // and cause the application to crash.
@@ -68,13 +104,16 @@ namespace GoogleApiExample
             int width = imageView.Height;
 
             //AC: workaround for not passing actual files
-            Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
+            //Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
+
+            //load picture from file
+            Android.Graphics.Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height);
 
             //convert bitmap into stream to be sent to Google API
             string bitmapString = "";
             using (var stream = new System.IO.MemoryStream())
             {
-                bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
+                bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 100, stream);
 
                 var bytes = stream.ToArray();
                 bitmapString = System.Convert.ToBase64String(bytes);
